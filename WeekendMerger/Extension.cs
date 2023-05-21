ddcbf579fe96e50d576be89516484111dec54280
BAKAOLC <ritsukage@qq.com>
2023-05-21 20:45:37 +0800

    Fix Shuffle

--- WeekendMerger/Extension.cs
+++ WeekendMerger/Extension.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WeekendMerger
 {
     public static class Extension
     {
-        public class RandomComparer<TKey> : IComparer<TKey>
-        {
-            readonly Random random = new(DateTime.Now.Millisecond);
-
-            public int Compare(TKey? x, TKey? y) => random.Next(3) - 1;
-        }
-
         public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source)
         {
-            return source.OrderBy(x => x, new RandomComparer<TSource>());
+            Random random = new(Guid.NewGuid().GetHashCode());
+            var array = source.ToArray();
+            for (int i = array.Length - 1; i > 0; i--)
+            {   
+                int j = random.Next(i + 1);
+                (array[i], array[j]) = (array[j], array[i]);
+            }
+            return array;
         }
     }
 }
